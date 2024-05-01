d3e1988683dba296bfa497208a6da7ea077a615c
Tiktokyi <tiktokyi@gmail.com>
2024-05-01 11:12:51 +0900

    Init : First Commit

---
+++ src/main/java/com/example/alwaysSpring/AlwaysSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.alwaysSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlwaysSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlwaysSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alwaysSpring/AlwaysSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alwaysSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlwaysSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
