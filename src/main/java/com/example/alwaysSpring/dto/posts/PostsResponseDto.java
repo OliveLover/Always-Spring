03c9d1d8d159b1c05b5b29410da3b2ed80ddfca8
Tiktokyi <tiktokyi@gmail.com>
2024-05-03 17:06:55 +0900

    Mod : add time to postsReseponseDto

--- src/main/java/com/example/alwaysSpring/dto/posts/PostsResponseDto.java
+++ src/main/java/com/example/alwaysSpring/dto/posts/PostsResponseDto.java
@@ -4,6 +4,8 @@ import com.example.alwaysSpring.domain.posts.Posts;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class PostsResponseDto {
@@ -11,11 +13,15 @@ public class PostsResponseDto {
     private String title;
     private String content;
     private String author;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
 
     public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor();
+        this.createdDate = entity.getCreatedDate();
+        this.modifiedDate = entity.getModifiedDate();
     }
 }
