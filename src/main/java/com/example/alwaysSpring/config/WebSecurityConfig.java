ec1585142cad5f9d006870ac1c06452fd2e04d6c
Tiktokyi <tiktokyi@gmail.com>
2024-05-10 11:24:40 +0900

    Create : create WebSecurityConfig, chaining CorsConfiguration

---
+++ src/main/java/com/example/alwaysSpring/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.alwaysSpring.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(false);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+//                        .anyRequest().authenticated()
+                                .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+        ;
+
+        return http.build();
+    }
+
+}
--- src/main/java/com/example/alwaysSpring/controller/PatientController.java
+++ src/main/java/com/example/alwaysSpring/controller/PatientController.java
@@ -11,25 +11,21 @@ import org.springframework.web.bind.annotation.*;
 public class PatientController {
     private final PatientService patientService;
 
-    @CrossOrigin(originPatterns = "*")
     @GetMapping("/api/v1/patients")
     public ResponseEntity<PatientsResponseDto> findAll() {
         return patientService.findAll();
     }
 
-    @CrossOrigin(originPatterns = "*")
     @PostMapping("/api/v1/patients")
     public ResponseEntity<PatientsRegisterResponseDto> register(@RequestBody PatientsRegisterRequestDto requestDto) {
         return patientService.register(requestDto);
     }
 
-    @CrossOrigin(originPatterns = "*")
     @PutMapping("/api/v1/patients/{id}")
     public ResponseEntity<PatientsUpdateResponseDto> update(@PathVariable Long id, @RequestBody PatientsUpdateRequestDto requestDto) {
         return patientService.update(id, requestDto);
     }
 
-    @CrossOrigin(originPatterns = "*")
     @PutMapping("/api/v1/patients/{id}/delete")
     public ResponseEntity<PatientsDeleteResponseDto> softDelete(@PathVariable Long id) {
         return patientService.softDelete(id);
